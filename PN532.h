6d173b63a9e59ff75fafa2544f686e54e42d5671
Dave Smith <dave.g.smith@gmail.com>
2013-09-27 12:03:39 +0100

    Eclipse stuff for the Arduino plugin

---
+++ PN532.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef PN532_H_
+#define PN532_H_
+#include "Arduino.h"
+//add your includes for the project PN532 here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project PN532 here
+
+
+
+
+//Do not add code below this line
+#endif /* PN532_H_ */
